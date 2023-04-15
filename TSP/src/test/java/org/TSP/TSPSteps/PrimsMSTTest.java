916ddc99bae3234a0d3eca3f00bbb97fc915ec00
Venkatesha Matam <matam.v@northeastern.edu>
2023-04-14 21:44:49 -0400

    PrimsMST unit test

--- TSP/src/test/java/org/TSP/TSPSteps/PrimsMSTTest.java
+++ TSP/src/test/java/org/TSP/TSPSteps/PrimsMSTTest.java
@@ -1,66 +1,98 @@
 package org.TSP.TSPSteps;
 
 import org.TSP.Graph.Edge;
+import org.TSP.Graph.Graph;
 import org.TSP.Graph.Vertex;
 import org.TSP.TSPsteps.PrimsMST;
 import org.junit.Assert;
 import org.junit.jupiter.api.Test;
 
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
+import java.util.*;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import org.TSP.Graph.Edge;
+import org.TSP.Graph.Vertex;
+import org.TSP.TSPsteps.PrimsMST;
+import org.junit.Before;
+
+import java.util.*;
+
+import static org.junit.Assert.assertEquals;
+
 public class PrimsMSTTest {
+    private HashMap<Vertex, List<Edge>> graph;
+    private Vertex v1;
+    private Vertex v2;
+    private Vertex v3;
+
+    @Before
+    public void setUp() {
+        // Create the vertices
+        v1 = new Vertex("London", 51.5074, -0.1278);
+        v2 = new Vertex("Paris", 48.8566, 2.3522);
+        v3 = new Vertex("Bangalore", 77.5946, 12.9716);
+
+        // Create the edges
+        Edge e1 = new Edge(v1, v2);
+        Edge e2 = new Edge(v1, v3);
+        Edge e3 = new Edge(v2, v3);
+
+        // Add the edges to the vertices
+        graph = new HashMap<>();
+        graph.put(v1, new ArrayList<>());
+        graph.put(v2, new ArrayList<>());
+        graph.put(v3, new ArrayList<>());
+        graph.get(v1).add(e1);
+        graph.get(v1).add(e2);
+        graph.get(v2).add(e1);
+        graph.get(v2).add(e3);
+        graph.get(v3).add(e2);
+        graph.get(v3).add(e3);
+    }
 
     @Test
     public void testPrim() {
-        // Create vertices
-        Vertex v1 = new Vertex("London", 51.5074, -0.1278);
-        Vertex v2 = new Vertex("Paris", 48.8566, 2.3522);
-        Vertex v3 = new Vertex("Bangalore", 77.5946, 12.9716);
-        Vertex v4 = new Vertex("New York", 40.7128, -74.0060);
-        // Create edges
+        v1 = new Vertex("London", 51.5074, -0.1278);
+        v2 = new Vertex("Paris", 48.8566, 2.3522);
+        v3 = new Vertex("Bangalore", 77.5946, 12.9716);
+
+        // Create the edges
         Edge e1 = new Edge(v1, v2);
-        Edge e2 = new Edge(v2, v3);
-        Edge e3 = new Edge(v3, v4);
-        Edge e4 = new Edge(v4, v1);
-        Edge e5 = new Edge(v1, v3);
-        Edge e6 = new Edge(v2, v4);
+        Edge e2 = new Edge(v1, v3);
+        Edge e3 = new Edge(v2, v3);
 
-        // Create graph
         HashMap<Vertex, List<Edge>> graph = new HashMap<>();
-        graph.put(v1, new ArrayList<>(List.of(e1, e4, e5)));
-        graph.put(v2, new ArrayList<>(List.of(e1, e2, e6)));
-        graph.put(v3, new ArrayList<>(List.of(e2, e3, e5)));
-        graph.put(v4, new ArrayList<>(List.of(e3, e4, e6)));
-
-        // Run Prim's algorithm
+        graph.put(v1, new ArrayList<>());
+        graph.put(v2, new ArrayList<>());
+        graph.put(v3, new ArrayList<>());
+        graph.get(v1).add(new Edge(v1, v2));
+        graph.get(v1).add(new Edge(v1, v3));
+        graph.get(v2).add(new Edge(v2, v1));
+        graph.get(v2).add(new Edge(v2, v3));
+        graph.get(v3).add(new Edge(v3, v1));
+        graph.get(v3).add(new Edge(v3, v2));
         HashMap<Vertex, List<Edge>> mst = PrimsMST.prim(graph, v1);
 
-        // Verify MST edges
-        List<Edge> expectedEdges = new ArrayList<>();
-        expectedEdges.add(e1);
-        expectedEdges.add(e5);
-        expectedEdges.add(e4);
+        // Check that the MST contains all three vertices
+        assertEquals(3, mst.size());
+        assertTrue(mst.containsKey(v1));
+        assertTrue(mst.containsKey(v2));
+        assertTrue(mst.containsKey(v3));
 
-        List<Edge> actualEdges = mst.get(v1);
-        Assert.assertEquals(expectedEdges.size(), actualEdges.size());
-        for (Edge edge : expectedEdges) {
-            Assert.assertTrue(actualEdges.contains(edge));
+        // Check that the MST contains two edges
+        List<Edge> mstEdges = new ArrayList<>();
+        for (List<Edge> edges : mst.values()) {
+            mstEdges.addAll(edges);
         }
+        assertEquals(4, mstEdges.size());
 
-        actualEdges = mst.get(v2);
-        Assert.assertEquals(1, actualEdges.size());
-        Assert.assertTrue(actualEdges.contains(e1));
-
-        actualEdges = mst.get(v3);
-        Assert.assertEquals(1, actualEdges.size());
-        Assert.assertTrue(actualEdges.contains(e2));
-
-        actualEdges = mst.get(v4);
-        Assert.assertEquals(1, actualEdges.size());
-        Assert.assertTrue(actualEdges.contains(e4));
+        // Check that the MST weight is correct
+        double expectedWeight = 2* (new Edge(v1, v2).getWeight() + new Edge(v1, v3).getWeight());
+        double actualWeight = 0;
+        for (Edge edge : mstEdges) {
+            actualWeight += edge.getWeight();
+        }
+        assertEquals(expectedWeight, actualWeight, 0.0001);
     }
 }
