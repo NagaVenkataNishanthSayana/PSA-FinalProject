01db9dc427577e862c271cf5593ac150d044b094
Venkatesha Matam <matam.v@northeastern.edu>
2023-04-16 06:34:31 -0400

    Added simulatedAnnealing

--- TSP/src/main/java/org/TSP/Main.java
+++ TSP/src/main/java/org/TSP/Main.java
@@ -13,20 +13,21 @@ public class Main {
     public static void main(String[] args) {
         FileIO fileIO=new FileIO();
 
-
+        // Read file
         Graph graph=fileIO.getConnectedGraph();
         HashMap<Vertex, List<Edge>> graphMap=graph.getGraph();
-
+        // Start point
         Vertex start=graphMap.keySet().iterator().next();
         System.out.println("MST Start point:"+start.getId());
+        // Prims MST
         HashMap<Vertex,List<Edge>> minSpanTree= PrimsMST.prim(graphMap,start);
-
+        // Find odd vertices
         Set<Vertex> oddVertices= GraphUtils.findVerticesWithOddDegree(minSpanTree);
         System.out.println("odd vertices:" + oddVertices.size());
-
+        // Greedy Matching
         List<Edge> perfectMatchedEdges=GraphUtils.findPerfectMatching(graphMap,oddVertices);
         System.out.println("perfect matched edges:" + perfectMatchedEdges.size());
-
+        // MultiGraph
         HashMap<Vertex,List<Edge>> multiGraph= MultiGraph.formMultiGraph(minSpanTree,perfectMatchedEdges);
         System.out.println("multi graph:" + multiGraph.size());
         for (Vertex vertex : multiGraph.keySet()) {
@@ -35,17 +36,32 @@ public class Main {
                 System.out.println(edge.getDestination().getId());
             }
         }
-
+        // TSP
         List<Vertex>hamiltonCircuit=TSPSolver.solve(graphMap,multiGraph);
         System.out.println("TSP Weight:" +GraphUtils.calculateTotalDistance(hamiltonCircuit,graphMap));
-
-        long startTime = System.nanoTime();
-
+        // TwoOpt
+        long startTime1 = System.nanoTime();
+        List<Vertex> twoOptPath= TwoOpt.twoOpt(hamiltonCircuit,graphMap,100,100);
+        System.out.println("TwoOpt:"+GraphUtils.calculateTotalDistance(twoOptPath,graphMap));
+        long endTime1 = System.nanoTime();
+        long timeElapsed1 = endTime1 - startTime1;
+        double secondsElapsed1 = (double) timeElapsed1 / 1_000_000_000.0;
+        System.out.println("Time taken by TwoOpt method to execute (in seconds): " + secondsElapsed1);
+        // ThreeOpt
+        long startTime2 = System.nanoTime();
         List<Vertex> threeOptPath= ThreeOpt.threeOpt(hamiltonCircuit,graphMap);
         System.out.println("ThreeOpt:"+GraphUtils.calculateTotalDistance(threeOptPath,graphMap));
+        long endTime2 = System.nanoTime();
+        long timeElapsed2 = endTime2 - startTime2;
+        double secondsElapsed2 = (double) timeElapsed2 / 1_000_000_000.0;
+        System.out.println("Time taken by ThreeOpt method to execute (in seconds): " + secondsElapsed2);
 
-        long endTime = System.nanoTime();
-        long timeElapsed = endTime - startTime;
-        double secondsElapsed = (double) timeElapsed / 1_000_000_000.0;
-        System.out.println("Time taken by ThreeOpt method to execute (in seconds): " + secondsElapsed);
-}}
+        // Simulated Annealing
+        long startTime3 = System.nanoTime();
+        List<Vertex> annealedTour = SimulatedAnnealing.simulatedAnnealing(hamiltonCircuit, graphMap, 500, 0.001, 500000);
+        System.out.println("Simulated Annealing: " + GraphUtils.calculateTotalDistance(annealedTour, graphMap));
+        long endTime3 = System.nanoTime();
+        long timeElapsed3 = endTime3 - startTime3;
+        double secondsElapsed3 = (double) timeElapsed3 / 1_000_000_000.0;
+        System.out.println("Time taken by Simulated Annealing method to execute (in seconds): " + secondsElapsed3);
+    }}
---
+++ TSP/src/main/java/org/TSP/TSPsteps/SimulatedAnnealing.java
@@ -0,0 +1,48 @@
+package org.TSP.TSPsteps;
+
+import org.TSP.Graph.Vertex;
+import org.TSP.Graph.Edge;
+import org.TSP.util.GraphUtils;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Random;
+
+public class SimulatedAnnealing {
+
+    public static List<Vertex> simulatedAnnealing(List<Vertex> tour, HashMap<Vertex, List<Edge>> graph, double initialTemp, double coolingRate, int maxIterations) {
+        double temperature = initialTemp;
+        double bestDistance = GraphUtils.calculateTotalDistance(tour, graph);
+        List<Vertex> bestTour = new ArrayList<>(tour);
+
+        Random random = new Random();
+
+        for (int i = 0; i < maxIterations; i++) {
+            int index1 = random.nextInt(tour.size());
+            int index2 = random.nextInt(tour.size());
+
+            List<Vertex> newTour = TwoOpt.reverseSublist(tour, Math.min(index1, index2), Math.max(index1, index2));
+            double newDistance = GraphUtils.calculateTotalDistance(newTour, graph);
+
+            if (shouldAcceptMove(bestDistance, newDistance, temperature)) {
+                tour = newTour;
+                bestDistance = newDistance;
+                bestTour = new ArrayList<>(newTour);
+            }
+
+            temperature *= (1 - coolingRate);
+        }
+
+        return bestTour;
+    }
+
+    private static boolean shouldAcceptMove(double currentDistance, double newDistance, double temperature) {
+        if (newDistance < currentDistance) {
+            return true;
+        } else {
+            double probability = Math.exp((currentDistance - newDistance) / temperature);
+            return Math.random() < probability;
+        }
+    }
+}
